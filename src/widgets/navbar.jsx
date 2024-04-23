a2d9e34ced1f52f5a2e0a8b01c069fff91247442
unknown <thitiwatipst@icloud.com>
2024-04-23 16:23:42 +0700

    edit image path

--- src/widgets/navbar.jsx
+++ src/widgets/navbar.jsx
@@ -3,7 +3,7 @@ const NavBar = () => {
     return (
         <div className="sticky top-0 grid lg:grid-rows gap-5 w-fit ">
             <div className="mt-30">
-                <span><Link to='/MKIB/'><img className="mt-30max-w-60" src="/assets/logo/int_bee_logo.png"></img></Link></span>
+                <span><Link to='/MKIB/'><img className="mt-30max-w-60" src="./assets/logo/int_bee_logo.png"></img></Link></span>
             </div>
             <div className="text-xl font-bold max-w-60 text-center"> MWIT-KVIS Integration Bee Archive </div>
             <button className="btn-primary  max-w-60  hover:font-semibold"><Link to='/MKIB/hall-of-Fame'>Hall of Fame</Link></button>
