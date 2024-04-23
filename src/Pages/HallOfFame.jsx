63169448a68839c5662c747bac8d014f90a51643
unknown <thitiwatipst@icloud.com>
2024-04-23 17:29:02 +0700

    add hall of frame

--- src/Pages/HallOfFame.jsx
+++ src/Pages/HallOfFame.jsx
@@ -9,7 +9,21 @@ function HallOfFame() {
             <div className="sticky top-0">
                 <NavBar />
             </div>
-            <div>Under construction</div>
+            <div className="grid-cols gap-3">
+                <div className="text-3xl text-center font-bold">Hall of frame</div>
+                <div className="text-2xl font-bold">2023 Results</div>
+
+                The first MWIT-KVIS Integration Bee took place on Sunday, November 12 from 8:40 to 10:30 in Rooms 222 and 224 at KVIS. 
+                    
+                    <div><span className="font-bold">Winner:</span> Puttisan Korsettarat, KVIS</div>
+                    <div><span className="font-bold">Runner-up:</span> Werawis Asawapiromz, KVIS</div>
+                    <div><span className="font-bold">Semifinalists:</span> Dechatorn Pattasopon, MWIT; Nopparuj Sodsri, KVIS</div>
+                    <div><span className="font-bold">Quarterfinalists: </span> Kasidet Joohong, Nattapat Kingkan, Preechakorn Yasamorn, MWIT;
+                        Thanwa Ruchakhom, KVIS
+                    </div>
+                    
+                    <div className="w-full flex  items-center justify-center py-2"><img src="./assets/images/2023_group_photo.jpg" className="object-cover w-4/6"></img></div>
+            </div>
         </div>
         </div>
     )
